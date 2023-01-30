49e154edec5b5efe26047e89d93b226cab0cb9a1
Supasak <ksupasak@gmail.com>
2023-01-30 10:20:25 +0700

    update

--- app/controllers/esm_document_controller.rb
+++ app/controllers/esm_document_controller.rb
@@ -148,7 +148,7 @@ class EsmDocumentController < EsmController
   end
   def relation_has_many_remove
     
-      render :text=>"a = $('\##{params[:id]}'); a.slideUp('fast',function(){a.remove()}); " 
+      render :plain=>"a = $('\##{params[:id]}'); a.slideUp('fast',function(){a.remove()}); " 
       # @document = Document.find(params[:id])
       # @field = @document.find_field params[:f_id]
       # @record = @document.project.load_model[@document.table.name.to_sym].find(params[:r_id])
